b0a5e60d48206a396f5a0033a62d9f59fe667775
Daniele Zannotti <d.zannotti@me.com>
2025-03-23 21:56:52 +0000

    more file breakup

--- internal/core/creature/creature.go
+++ internal/core/creature/creature.go
@@ -16,26 +16,6 @@ type Creature struct {
 	log          *log.EventLog
 }
 
-func (c Creature) Name() string {
-	return c.name
-}
-
-func (c Creature) HitPoints() int {
-	return c.hitPoints
-}
-
-func (c Creature) MaxHitPoints() int {
-	return c.maxHitPoints
-}
-
-func (c Creature) Team() team.Team {
-	return c.team
-}
-
-func (c Creature) ActionPoints() int {
-	return c.actionPoints
-}
-
 func RollDice(sides int) int {
 	return rand.Intn(sides) + 1
 }
@@ -58,10 +38,6 @@ func (c *Creature) Attack(target *Creature) {
 		target.Die()
 	}
 	c.log.End()
-}
-
-func (c *Creature) IsDead() bool {
-	return c.hitPoints == 0
 }
 
 func (c *Creature) StartTurn() {
--- internal/core/encounter/encounter.go
+++ internal/core/encounter/encounter.go
@@ -9,54 +9,37 @@ import (
 
 type Act = func(active *creature.Creature, creatures []*creature.Creature, wg *sync.WaitGroup)
 
-func IsOver(creatures []*creature.Creature) bool {
-	playersAlive := false
-	enemiesAlive := false
-	for _, c := range creatures {
-		if !c.IsDead() {
-			if c.Team() == team.Player {
-				playersAlive = true
-			}
-			if c.Team() == team.Enemy {
-				enemiesAlive = true
-			}
-		}
-	}
-	return !playersAlive || !enemiesAlive
+func playTurn(log *log.EventLog, turn int, active *creature.Creature, creatures []*creature.Creature, act Act) {
+	log.Start(NewTurnEvent(turn+1, active))
+	defer log.End()
+	turnWG := sync.WaitGroup{}
+	turnWG.Add(1)
+	active.StartTurn()
+	go act(active, creatures, &turnWG)
+	turnWG.Wait()
 }
 
-func winner(creatures []*creature.Creature) team.Team {
+func playRound(log *log.EventLog, round int, creatures []*creature.Creature, act Act) {
+	log.Start(NewRoundEvent(round+1, creatures))
+	defer log.End()
+	turn := 0
 	for i := range creatures {
-		if !creatures[i].IsDead() {
-			return creatures[i].Team()
+		var active = creatures[i]
+		turn = turn + 1
+		playTurn(log, turn, active, creatures, act)
+		if IsOver(creatures) {
+			break
 		}
 	}
-	return team.None
 }
 
 func Play(log *log.EventLog, creatures []*creature.Creature, act Act, result chan team.Team) {
-	turn := 0
 	round := 0
 	log.Start(NewEncounterEvent(creatures))
 	defer log.End()
 	for !IsOver(creatures) {
-		log.Start(NewRoundEvent(round+1, creatures))
-		for i := range creatures {
-			var active = creatures[i]
-			log.Start(NewTurnEvent(turn+1, active))
-			turnWG := sync.WaitGroup{}
-			turnWG.Add(1)
-			active.StartTurn()
-			go act(active, creatures, &turnWG)
-			turnWG.Wait()
-			log.End() // turn
-			turn = turn + 1
-			if IsOver(creatures) {
-				break
-			}
-		}
+		playRound(log, round, creatures, act)
 		round = round + 1
-		turn = 0
 		log.End() // round
 	}
 	result <- winner(creatures)
