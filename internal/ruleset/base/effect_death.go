0143f05aea4c486e284e9872e41e0a3f6c5f95d4
Daniele Zannotti <d.zannotti@me.com>
2025-04-03 01:41:45 +0100

    linting

--- internal/ruleset/base/effect_death.go
+++ internal/ruleset/base/effect_death.go
@@ -2,9 +2,9 @@ package base
 
 import "anvil/internal/core"
 
-func NewDeathEffect(a *core.Actor) *core.Effect {
+func NewDeathEffect(_ *core.Actor) *core.Effect {
 	fx := &core.Effect{Name: "Death", Priority: core.PriorityLast}
-	fx.WithAfterTakeDamage(func(e *core.Effect, s *core.AfterTakeDamageState) {
+	fx.WithAfterTakeDamage(func(_ *core.Effect, s *core.AfterTakeDamageState) {
 		if s.Source.HitPoints == 0 {
 			s.Source.Die()
 		}
