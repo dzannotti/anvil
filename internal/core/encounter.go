41cd211681522f0c08ef465f9108b84f186995c7
Daniele Zannotti <d.zannotti@me.com>
2025-04-18 23:11:28 +0200

    Refactor api to be client callable (#31)

--- internal/core/encounter.go
+++ internal/core/encounter.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"slices"
-	"sync"
 )
 
 type Encounter struct {
@@ -14,48 +13,50 @@ type Encounter struct {
 	World           *World
 }
 
-type Act = func(active *Actor)
-
-func (e *Encounter) playTurn(act Act) {
-	e.Log.Start(TurnType, TurnEvent{Turn: e.Turn, Actor: e.ActiveActor()})
-	defer e.Log.End()
-	turnWG := sync.WaitGroup{}
-	turnWG.Add(1)
-	e.ActiveActor().StartTurn()
-	go func() {
-		defer turnWG.Done()
-		act(e.ActiveActor())
-	}()
-	e.ActiveActor().EndTurn()
-	turnWG.Wait()
-}
-
-func (e *Encounter) playRound(act Act) {
-	e.Log.Start(RoundType, RoundEvent{Round: e.Round, Actors: e.Actors})
-	defer e.Log.End()
-	e.Turn = 0
-	for i := range e.InitiativeOrder {
-		e.Turn = i
-		e.playTurn(act)
-		if e.IsOver() {
-			break
-		}
-	}
-}
-
-func (e *Encounter) Play(act Act) string {
-	e.Round = 0
-	e.Turn = 0
+func (e *Encounter) Start() {
 	for _, a := range e.Actors {
 		a.Encounter = e
 	}
 	e.InitiativeOrder = slices.Clone(e.Actors)
 	e.Log.Start(EncounterType, EncounterEvent{Actors: e.Actors, World: e.World})
-	defer e.Log.End()
-	for !e.IsOver() {
-		e.playRound(act)
-		e.Round = e.Round + 1
+	e.Round = -1
+	e.startRound()
+	e.startTurn()
+}
+
+func (e *Encounter) End() {
+	if !e.IsOver() {
+		return
 	}
-	winner, _ := e.Winner()
-	return winner
+	e.Log.End()
+}
+
+func (e *Encounter) EndTurn() {
+	e.ActiveActor().EndTurn()
+	e.Log.End()
+	if e.IsOver() {
+		e.endRound()
+		return
+	}
+	e.Turn = e.Turn + 1
+	if e.Turn >= len(e.InitiativeOrder) {
+		e.endRound()
+		e.startRound()
+	}
+	e.startTurn()
+}
+
+func (e *Encounter) startRound() {
+	e.Round = e.Round + 1
+	e.Log.Start(RoundType, RoundEvent{Round: e.Round, Actors: e.Actors})
+	e.Turn = 0
+}
+
+func (e *Encounter) endRound() {
+	e.Log.End()
+}
+
+func (e *Encounter) startTurn() {
+	e.Log.Start(TurnType, TurnEvent{Turn: e.Turn, Actor: e.ActiveActor()})
+	e.ActiveActor().StartTurn()
 }
---
+++ internal/demo/demo.go
@@ -0,0 +1,70 @@
+package demo
+
+import (
+	"anvil/internal/core"
+	"anvil/internal/core/stats"
+	"anvil/internal/core/tags"
+	"anvil/internal/eventbus"
+	"anvil/internal/grid"
+	"anvil/internal/ruleset"
+	"anvil/internal/ruleset/fighter"
+	"anvil/internal/ruleset/item/armor"
+	"anvil/internal/ruleset/item/weapon"
+	"anvil/internal/ruleset/monster/undead/zombie"
+	"anvil/internal/tag"
+)
+
+func setupWorld(world *core.World) {
+	walls := make([]grid.Position, 0, 256)
+	for x := range world.Width() {
+		walls = append(walls,
+			grid.Position{X: x, Y: 0},
+			grid.Position{X: x, Y: world.Height() - 1})
+	}
+
+	for y := range world.Height() {
+		walls = append(walls,
+			grid.Position{X: 0, Y: y},
+			grid.Position{X: world.Width() - 1, Y: y})
+	}
+	limit := min(world.Width()-3, world.Height()-3)
+	for y := 1; y < limit; y++ {
+		walls = append(walls, grid.Position{X: world.Width() - 1 - y, Y: y})
+	}
+	for _, p := range walls {
+		if cell, ok := world.At(p); ok {
+			cell.Tile = core.Wall
+		}
+	}
+}
+
+func Create(hub *eventbus.Hub) (*core.World, *core.Encounter) {
+	world := core.NewWorld(10, 10)
+	setupWorld(world)
+
+	/*wres := core.Resources{Max: map[tag.Tag]int{
+		tags.WalkSpeed:  5,
+		tags.SpellSlot1: 1,
+	}}
+
+	wizard := ruleset.NewPCActor(&hub, world, grid.Position{X: 2, Y: 2}, "Wizard", 8, stats.Attributes{Strength: 10, Dexterity: 15, Constitution: 14, Intelligence: 16, Wisdom: 12, Charisma: 8}, stats.Proficiencies{Bonus: 2}, wres)
+	wizard.Equip(weapon.NewDagger())*/
+
+	cres := core.Resources{Max: map[tag.Tag]int{
+		tags.WalkSpeed: 5,
+	}}
+	cedric := ruleset.NewPCActor(hub, world, grid.Position{X: 3, Y: 2}, "Cedric", 12, stats.Attributes{Strength: 16, Dexterity: 13, Constitution: 14, Intelligence: 8, Wisdom: 14, Charisma: 10}, stats.Proficiencies{Bonus: 2}, cres)
+	cedric.Equip(weapon.NewGreatAxe())
+	cedric.Equip(armor.NewChainMail())
+	cedric.AddEffect(fighter.NewFightingStyleDefense())
+	cedric.AddProficiency(tags.MartialWeapon)
+	mob1 := zombie.New(hub, world, grid.Position{X: 7, Y: 6}, "Zombie 1")
+	mob2 := zombie.New(hub, world, grid.Position{X: 7, Y: 7}, "Zombie 2")
+	//mob3 := zombie.New(hub, world, grid.Position{X: 6, Y: 6}, "Zombie 3")
+	encounter := &core.Encounter{
+		Log:    hub,
+		World:  world,
+		Actors: []*core.Actor{ /*wizard, */ cedric, mob1, mob2 /* mob3*/},
+	}
+	return world, encounter
+}
