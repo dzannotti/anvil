e46c0091157ff14da75406ff172c4717d941c8ff
Daniele Zannotti <d.zannotti@me.com>
2025-04-21 02:25:08 +0200

    ci fix?

--- internal/prettyprint/prettyprint.go
+++ internal/prettyprint/prettyprint.go
@@ -234,18 +234,18 @@ func printExpressionResult(e core.ExpressionResultEvent) string {
 }
 
 func printSavingThrowResult(e core.SavingThrowResultEvent) string {
-    return formatRollResult(e.Success, e.Critical, e.Value, e.Against)
+	return formatRollResult(e.Success, e.Critical, e.Value, e.Against)
 }
 
 func formatRollResult(success, critical bool, value, against int) string {
-    icons := map[bool]string{true: "✅", false: "❌"}
-    sb := strings.Builder{}
-    sb.WriteString(icons[success])
-    if critical {
-        sb.WriteString("💥 Critical")
-    }
-    sb.WriteString(map[bool]string{true: " Success", false: " Failure"}[success])
-    return fmt.Sprintf("%s %d vs %d", sb.String(), value, against)
+	icons := map[bool]string{true: "✅", false: "❌"}
+	sb := strings.Builder{}
+	sb.WriteString(icons[success])
+	if critical {
+		sb.WriteString("💥 Critical")
+	}
+	sb.WriteString(map[bool]string{true: " Success", false: " Failure"}[success])
+	return fmt.Sprintf("%s %d vs %d", sb.String(), value, against)
 }
 
 func printCheckResult(e core.CheckResultEvent) string {
